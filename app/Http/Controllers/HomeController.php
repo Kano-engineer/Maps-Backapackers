998e21b1e6df09b4958cba58b336365c01955013
Hide-cloud <hideyama24111112@gmail.com>
2021-01-05 22:00:24 +0900

    create profile image

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -46,6 +46,31 @@ class HomeController extends Controller
         return view('myprofile');
     }
 
+
+    //プロフィール画面から画像をアップ
+    public function storeMyImg(Request $request)
+    {
+        //画像ファイルに名前をつけて指定ディレクトリに保存、変数に代入
+        //postで受け取ったデータ（$request）の中にある myPic(ネーム属性)を、第二引数”ユーザーid＋日付.jpg”の名前で第一引数のディレクトリに保存
+        $filepath = $request->myPic->storeAs('public/profile_images', Auth::id() . date("YmdHis"). '.jpg');
+        
+        //ユーザーIDからユーザー情報を取得、変数に代入
+        $user = User::find(auth()->id());
+
+        //ユーザー情報からmy_picカラムのデータをピックアップし、
+        //$filepathのファイル名の部分のみをmy_picカラムに代入
+        $user->my_pic = basename($filepath);
+
+        //保存
+        $user->save();
+
+        //ルート名 showMypage へ移動。フラッシュメッセージのデータも一緒に。
+        return redirect()->route('showMyProfile')->with('success', '新しいプロフィールを登録しました');
+
+        //return view('myprofile');
+    }
+
+
     //ユーザープロフィール画面を表示
     public function showUserProfile($id)
     {
