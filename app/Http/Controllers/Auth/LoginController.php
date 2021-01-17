577ff233f1b062f22e79e9411ecf778a324ad5b9
Kano <takayukikano.0613@gmail.com>
2021-01-17 22:36:14 +0900

    first commit

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = RouteServiceProvider::HOME;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+
+    
+    public function guestLogin(){
+
+    $email = 'hoge@hoge.jp';
+    $password = 'hogehoge';
+
+    if (Auth::attempt(['email' => $email, 'password' => $password])) {
+        return redirect()->route('home');
+    }
+
+    return redirect('/');
+    }
+
+    
+}
