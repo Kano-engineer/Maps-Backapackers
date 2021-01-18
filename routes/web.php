8145164ead91d155a78f78c62e2f5898ade13e74
Kano <takayukikano.0613@gmail.com>
2021-01-18 14:57:31 +0900

    guest login

--- routes/web.php
+++ routes/web.php
@@ -22,12 +22,4 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
 Route::get('/login/guest', 'Auth\LoginController@guestLogin');
