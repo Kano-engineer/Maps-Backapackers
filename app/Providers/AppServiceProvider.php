8e6e531fdc5df0e44f33c5051fef37c02f5c9701
Kano <takayukikano.0613@gmail.com>
2021-01-29 11:48:12 +0900

    make it better

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,8 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //Heroku
-        if (\App::environment('production')) {
-            \URL::forceScheme('https');
+        //
     }
 }
