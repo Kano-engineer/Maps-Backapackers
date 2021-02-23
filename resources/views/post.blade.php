bcedac533b753a5a9b7992f3ac7056e347eb6098
Kano <takayukikano.0613@gmail.com>
2021-02-23 12:58:37 +0900

    Font

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -6,14 +6,25 @@
     <title>チャット</title>
     <link href="{{ mix('css/app.css') }}" rel="stylesheet" type="text/css">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <style>
+ 
+    </style>
 </head>
  
 <body>
+
     <div id="app">
         <example-component></example-component>
     </div>
  
     <script src="{{ mix('js/app.js') }}"></script>
+    
+    <footer>
+<!--フッターの記述-->
+    </footer>
 </body>
+
+
  
 </html>
