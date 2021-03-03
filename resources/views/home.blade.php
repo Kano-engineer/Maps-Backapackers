218740dcc50c8372963920fc4667209b66d00946
Kano <takayukikano.0613@gmail.com>
2021-03-03 21:06:31 +0900

    Update:Use card for layout of post

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,14 +15,8 @@
                             <a href="/profile"><img style="" src="{{ asset('storage/' . $image['file_name']) }}" class="card-img-top" alt="..."></a>
                             @endforeach
                         @endif
-                        <a href="/profile" class="card-body">
-                        <h4><i class="fas fa-user">USER：{{ Auth::user()->name }}</i></h4>
-                            @foreach ($comment as $comment)
-                               <p class="card-text">{{ $comment ->comment}}</p>
-                            @endforeach
-                        </a>
                         <ul class="list-group list-group-flush">
-                            
+                            <li class="list-group-item"><a href="/profile"><i class="fas fa-user">USER：{{ Auth::user()->name }}</i></a></li>
                             <li class="list-group-item"><a href="/map"><i class="fas fa-globe-europe">MAP SEARCH(実装中：クリックOK)</i></a></li>
                             <li class="list-group-item"><a href="/post"><i class="fas fa-comment-dots">TALK SPACE</i></a></li>
                         </ul>
@@ -59,7 +53,7 @@
                     <!-- TIMELINE -->
                     @foreach ($pins as $pins)
                     <div class="card">
-                            <h5 class="card-header" style="color:#094067;"><a type="button" class="btn btn-default btn-sm" style="color:#3da9fc;" href="profile/{{$pins->user_id}}"><i class="fas fa-user"></i> {{$pins->user->name}}</a>：{{ $pins->text }}</h5>
+                            <h5 class="card-header" style="color:#094067;"><a type="button" class="btn btn-default" style="color:#3da9fc;" href="/profile/{{$pins->user_id}}"><i class="fas fa-user">{{$pins->user->name}}</i></a><i class="fas fa-map-marker-alt">{{ $pins->text }}</i></h5>
                         <a href="post/{{$pins->id}}" class="card-body">
                             @if ($pins->photos->isEmpty()) 
                                     <img style="width:250px;height:200px;" src="{{ URL::asset('image/noimage.png') }}"  class="card-img-top" alt="...">
@@ -107,7 +101,7 @@
                     <!-- LIKES -->
                     @foreach ($user->favorites as $favorite)
                         <div class="card">
-                                <h5 class="card-header" style="color:#094067;"><a type="button" class="btn btn-default btn-sm" style="color:#3da9fc;" href="profile/{{$favorite->user_id}}"><i class="fas fa-user"></i> {{$favorite->user->name}}</a>：{{ $favorite->text }}</h5>
+                                <h5 class="card-header" style="color:#094067;"><a type="button" class="btn btn-default" style="color:#3da9fc;" href="/profile/{{$pins->user_id}}"><i class="fas fa-user">{{$favorite->user->name}}</i></a><i class="fas fa-map-marker-alt">{{ $favorite->text }}</i></h5>
                             <a href="post/{{$favorite->id}}" class="card-body">
                                 @if ($favorite->photos->isEmpty()) 
                                         <img style="width:250px;height:200px;" src="{{ URL::asset('image/noimage.png') }}"  class="card-img-top" alt="...">
