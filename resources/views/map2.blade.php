57e942c9233aa2bda68684ab22f914b71186e4e2
Kano <takayukikano.0613@gmail.com>
2021-03-25 20:45:39 +0900

    #75 To improve UX, add search function

---
+++ resources/views/map2.blade.php
@@ -0,0 +1,158 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Maps.Backpackers</title>
+    <meta charset="utf-8">
+    <style>
+    /* Responsive */
+    .map_wrapper {
+      position: relative; 
+      width:100%;
+      padding-top:56.25%;
+      border: 1px solid #CCC;  
+    }
+    .map_wrapper .gmap {
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      top: 0;
+      left: 0;
+    } 
+    </style>
+
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+</head>
+
+<body>
+  <div id="app">
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <div class="container">
+                <a class="navbar-brand" href="{{ url('/home') }}">
+                Maps.Backpackers
+                    <!-- {{ config('app.name', 'Laravel') }} -->
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav mr-auto">
+
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+
+                        <!-- Authentication Links -->
+                        @guest
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                                
+                            @endif
+                        @else
+                        <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+                                
+                                <a class="nav-link dropdown-toggle"  href="/profile"><i class="fas fa-user"></i>My Profile</a>
+
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+                                    
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <main class="py-4">
+             <!-- Show map -->
+             <div class="map_wrapper">
+                <div id="gmap" class="gmap"></div>
+             </div>
+        </main>
+    <a></a>
+  </div>
+
+<script>
+
+function initMap() {
+    
+    const pins = @json($pins);
+    for(let i in pins) {
+    console.log(pins[i].text);
+    }
+
+    var addresses = [
+        'アラスカ',
+        'バンコク',
+        '渋谷'
+    ];
+
+var latlng = []; //緯度経度の値をセット
+var marker = []; //マーカーの位置情報をセット
+var myLatLng; //地図の中心点をセット用
+var geocoder;
+geocoder = new google.maps.Geocoder();
+
+var map = new google.maps.Map(document.getElementById('gmap'));//地図を作成する
+
+geo(aftergeo);
+
+function geo(callback){
+    var cRef = addresses.length;
+    for (var i = 0; i < addresses.length; i++) {
+        (function (i) { 
+            geocoder.geocode({'address': addresses[i]}, 
+                function(results, status) { // 結果
+                    if (status === google.maps.GeocoderStatus.OK) { // ステータスがOKの場合
+                        latlng[i]=results[0].geometry.location;// マーカーを立てる位置をセット
+                        marker[i] = new google.maps.Marker({
+                            position: results[0].geometry.location, // マーカーを立てる位置を指定
+                            map: map // マーカーを立てる地図を指定
+                        });
+                    } else { // 失敗した場合
+                    }//if文の終了。ifは文なので;はいらない
+                    if (--cRef <= 0) {
+                        callback();//全て取得できたらaftergeo実行
+                    }
+                }//function(results, status)の終了
+            );//geocoder.geocodeの終了
+        }) (i);
+    }//for文の終了
+}//function geo終了
+
+function aftergeo(){
+    myLatLng = latlng[0];//最初の住所を地図の中心点に設定
+    var opt = {
+        center: myLatLng, // 地図の中心を指定
+        zoom: 2 // 地図のズームを指定
+    };//地図作成のオプションのうちcenterとzoomは必須
+    map.setOptions(opt);//オプションをmapにセット
+}//function aftergeo終了
+
+};//function initMap終了
+</script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCKeJI2_CkK91_yzwlmyIIrzVqyJj2CgdE&callback=initMap" async defer></script>
+</body>
+</html>
