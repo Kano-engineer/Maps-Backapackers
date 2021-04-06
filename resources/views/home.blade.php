0c8f54c418ad28cf8ca922ff300ab5a922f1683a
Kano <takayukikano.0613@gmail.com>
2021-04-06 12:10:23 +0900

    #56 To improve UI/UX, update sidebar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -145,9 +145,10 @@
                             <a href="/profile"><img style="border-radius: 50%;" src="{{ asset('storage/' . $image['file_name']) }}" class="card-img-top" alt="..."></a>
                             @endforeach
                         @endif
-                            <p></p>
-                            <a href="/profile" type="button" class="btn btn-primary"><i class="fas fa-user"> {{ Auth::user()->name }}</i></a>
-                            <p></p>
+                        <br>
+                        <h5 style="font-weight: bold; font-size: xxx-large; text-align: center;">{{ $user->name }}</h5>
+                            <!-- <a href="/profile" type="button" class="btn btn-primary"><i class="fas fa-user"> {{ Auth::user()->name }}</i></a>
+                            <p></p> -->
                             <!-- <a href="/post/" type="button" class="btn btn-primary"><i class="fas fa-comment-dots"></i>TALK</a>
                             <p></p> -->
                             <a href="/index/" type="button" class="btn btn-primary"><i class="fas fa-search"></i> SEARCH</a>
@@ -169,6 +170,7 @@
                         <div class="tab_content_description">
                             @if($pins->count())
                                 <table border="1">
+                                    <br>
                                     <!-- Show map -->
                                     <div class="map_wrapper">
                                         <div id="gmap" class="gmap" style="overflow: hidden;height: 100%; width: 100%; position: absolute; top: 0px; left: 0px; background-color: rgb(229, 227, 223);"></div>
