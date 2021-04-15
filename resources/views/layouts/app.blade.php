52773ab66aff6490dac9f5e1325f7ab2ac447bd6
Kano <takayukikano.0613@gmail.com>
2021-04-15 17:29:36 +0900

    test

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -94,7 +94,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/home') }}">
-                <i class="fas fa-home"></i> Maps.Backpackers
+                Maps.Backpackers
                     <!-- {{ config('app.name', 'Laravel') }} -->
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
