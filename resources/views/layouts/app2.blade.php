1ae3e4f4bedfc21d43d9568e01ca7f61174b3ef0
Kano <takayukikano.0613@gmail.com>
2021-04-16 11:27:09 +0900

    #56 To improve UI/UX, chenge HOME button

--- resources/views/layouts/app2.blade.php
+++ resources/views/layouts/app2.blade.php
@@ -92,7 +92,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/home') }}">
-                <i class="fas fa-home"></i> Maps.Backpackers
+                <i class="fas fa-home"></i> HOME
                     <!-- {{ config('app.name', 'Laravel') }} -->
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
